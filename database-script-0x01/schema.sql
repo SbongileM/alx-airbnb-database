38675302a0bbf91be862aeca722b35699c0fa6df
SbongileM <sbongilep4@gmail.com>
2025-06-29 06:18:40 +0200

    Add property table creation query

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE user (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT unique_email UNIQUE (email)
 );
+
+-- Property table
+CREATE TABLE property (
+    property_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    host_id UUID NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    price_per_night DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT FK_property_host FOREIGN KEY (host_id) REFERENCES "user"(user_id)
+);
